ef45090df5574e444b314305fc43882b3f511e0e
Pablo Martínes Quesada <pabmar31@ucm.es>
2022-03-14 12:25:42 +0100

    Archivos iniciales Repositorio Local

---
+++ Práctica/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Práctica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

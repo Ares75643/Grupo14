aa9682614e2886eaa910703e5148b866f3bdb268
Dorjee Khampa Herrezuelo Blasco <dherrezu@ucm.es>
2022-03-21 19:51:39 +0100

    colores nuevos

--- Práctica/Program.cs
+++ Práctica/Program.cs
@@ -4,11 +4,14 @@ namespace Práctica
 {
     class Program
     {
+        enum Color { Red, Yellow, Cyan, Magenta, Blue, Green }; 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
             //gioaujdsaghkajgcadwawa
+
+
         }
     }
 }
